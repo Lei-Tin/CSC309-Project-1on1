948eba2f5cb3cfbade735c0714d3e1aaa84015b0
WilsoZhang16 <wilso.zhang@mail.utoronto.ca>
2024-03-08 16:20:05 -0500

    models update

--- contacts/models.py
+++ contacts/models.py
@@ -2,16 +2,12 @@ from django.db import models
 
 
 # Create your models here.
-class ContactList(models.Model):
-    owner = models.OneToOneField("auth.user", on_delete=models.CASCADE)
-
-
-class Contains(models.Model):
-    member = models.ManyToManyField("auth.user", on_delete=models.CASCADE, related_name='member_of')
-    contact_list = models.ForeignKey(ContactList, on_delete=models.CASCADE)
+class Contacts(models.Model):
+    requester = models.OneToOneField("auth.user", on_delete=models.CASCADE)
+    requested = models.OneToOneField("auth.user", on_delete=models.CASCADE)
     accepted = models.BooleanField(default=False)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['contact_list', 'member'], name='membership')
+            models.UniqueConstraint(fields=['requester', 'requested'], name='membership')
         ]
