c76fd346a7e026c0d9ee755f858ed41802c3e372
HLeiTR <sy.hung@mail.utoronto.ca>
2024-03-09 13:23:48 -0500

    [*] Fixed model not found error

--- calendars/views.py
+++ calendars/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-from models import Calendar, Availability, Invitee, Meets
+from .models import Calendar, Availability, Invitee, Meets
 
 # Create your views here.
 class calendarListView(TemplateView):
