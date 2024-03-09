95b7446c184bc7a1423a134d476cfd1ddc3b8b3f
HLeiTR <sy.hung@mail.utoronto.ca>
2024-03-08 23:59:36 -0500

    [+] Added API calls for getting CalendarStatus and getting MeetingStatus

--- calendars/views.py
+++ calendars/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from models import Calendar, Availability, Invitee, Meets
+
 # Create your views here.
 class calendarListView(TemplateView):
     def get(self, request):
@@ -32,6 +38,122 @@ class calendarSelectionView(TemplateView):
     def post(self, request):
         # TODO: Add logic to this
         return HttpResponse('POST request')
+    
+
+# API Views for Status
+# Calls for obtaining the status of the current calendar's availbility situation
+class CalendarStatus(APIView):
+    """
+    This API Call is used to obtain information about the current calendar's filled information
+
+    Currently POST is not supported, only GET
+    """
+    def get(self, request, calendar_id):
+        """
+        It should return all of the users' filled information given a calendar_id
+        """
+        # Check if the calendar_id passed in the URL is valid
+        # If it is not valid, return a 404
+        if not Calendar.objects.filter(id=calendar_id).exists():
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        # It should return a dict of the following structure: 
+        """
+        {
+            'availiability': [
+                # A list of dictionaries containing all users' availabilities within this calendar
+                {
+                    'user_id': id,
+                    'start_period': start_time,
+                    'end_period': end_time,
+                    'preference': preference
+                },
+                ...
+            ],
+            'invitees': [
+                # A list of dictionaries containing all users that are invited to the calendar, 
+                # including their current acceptance statuses
+                {
+                    'user_id': id,
+                    'deadline': deadline
+                    'status': status  # We compute the status by checking if the user is in the availability list
+                },
+                ...
+            ],
+            'owner': owner_id
+        }
+        """
+
+        d = {
+            'owner': Calendar.objects.get(id=calendar_id).owner.id, 
+             'availiability': [], 
+             'invitees': []
+             }
+        
+        # Convert every entry in the queryset to a dictionary
+        for availability in Availability.objects.filter(calendar_id=calendar_id):
+            d['availiability'].append({
+                'user_id': availability.user.id,
+                'start_period': availability.start_period,
+                'end_period': availability.end_period,
+                'preference': availability.preference
+            })
+
+        accepted_users = set()
+        for availability in Availability.objects.filter(calendar_id=calendar_id):
+            accepted_users.add(availability.user.id)
+
+        for invitee in Invitee.objects.filter(calendar_id=calendar_id):
+            d['invitees'].append({
+                'user_id': invitee.invitee.id,
+                'deadline': invitee.deadline,
+                'status': invitee.invitee in accepted_users
+            })
+
+        return Response(d)
+
+
+class MeetingStatus(APIView):
+    """
+    This API Call is used to obtain information about the current calendar's meeting status
+    Already finalized meeting times for everyone in the calendar
+
+    Currently POST is not supported, only GET
+    """
+    def get(self, request, calendar_id):
+        # Check if the calendar_id passed in the URL is valid
+        # If it is not valid, return a 404
+        if not Calendar.objects.filter(id=calendar_id).exists():
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        # It should return a dict of the following structure: 
+        """
+        {
+            'meets': [
+                # A list of dictionaries containing all users' meets within this calendar
+                {
+                    'user_id': id,
+                    'start_period': start_time,
+                    'end_period': end_time
+                },
+                ...
+            ]
+        }
+        """
+
+        d = {'meets': []}
+
+        # Convert every entry in the queryset to a dictionary
+        for meet in Meets.objects.filter(calendar_id=calendar_id):
+            d['meets'].append({
+                'user_id': meet.meeter.id,
+                'start_period': meet.start_period,
+                'end_period': meet.end_period
+            })
+
+        return Response(d)
+
+
     
 # TODO: Maybe consider adding views that are used as API endpoints to fetch information like:
 # Invited user's statuses with the calendar
