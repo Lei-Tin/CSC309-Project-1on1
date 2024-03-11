f0e7e70b2985959db5e6f3a09df0a0c8c8fc3952
yyuan-qin <113566884+yyuan-qin@users.noreply.github.com>
2024-03-11 19:50:13 -0400

    Revised profile: allow users to change password, completed api endpoint descriptions

--- OneOnOne/accounts/serializers.py
+++ OneOnOne/accounts/serializers.py
@@ -113,23 +113,27 @@ class ProfileSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=False)
     user = UserSerializer(read_only=True)
     profile_picture = serializers.ImageField(allow_null=True, required=False)
+    current_password = serializers.CharField(write_only=True, required=False)
+    new_password = serializers.CharField(write_only=True, required=False)
+    confirm_password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = Profile
-        fields = ['user', 'profile_picture', 'first_name', 'last_name', 'email']
+        fields = ['user', 'profile_picture', 'first_name', 'last_name', 'email',
+                  'current_password', 'new_password', 'confirm_password']
 
     def validate(self, data):
-        user = self.instance.user
-
         current_password = data.get('current_password')
         new_password = data.get('new_password')
         confirm_password = data.get('confirm_password')
 
         if current_password and new_password and confirm_password:
-            if not user.check_password(current_password):
-                raise serializers.ValidationError({'current_password': 'Current password is incorrect'})
+            if not self.instance.user.check_password(current_password):
+                raise serializers.ValidationError('Current password is incorrect')
             if new_password != confirm_password:
-                raise serializers.ValidationError({'new_password': 'Passwords must match'})
+                raise serializers.ValidationError('Password must match')
+        elif new_password or confirm_password:
+            raise serializers.ValidationError('Enter current password first')
 
         return data
 
@@ -140,8 +144,10 @@ class ProfileSerializer(serializers.ModelSerializer):
         user.last_name = validated_data.get('last_name', user.last_name)
         user.email = validated_data.get('email', user.email)
 
-        if 'new_password' in validated_data and validated_data.get('new_password'):
-            user.set_password(validated_data['new_password'])
+        new_password = validated_data.get('new_password')
+
+        if new_password:
+            user.set_password(new_password)
         user.save()
 
         instance.profile_picture = validated_data.get('profile_picture', instance.profile_picture)
