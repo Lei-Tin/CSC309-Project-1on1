b7a57b9086432972d29cc6a5b68d72bd258ffd2f
HLeiTR <sy.hung@mail.utoronto.ca>
2024-03-10 20:34:35 -0400

    [-] Remvoed JS as a static file directory

--- OneOnOne/one_on_one/settings.py
+++ OneOnOne/one_on_one/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),
-                    os.path.join(STATIC_ROOT, 'javascript/'),
+                    # os.path.join(STATIC_ROOT, 'javascript/'),
                     os.path.join(STATIC_ROOT, 'images/')
                   )
 
