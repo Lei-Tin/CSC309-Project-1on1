3eb0ee96b434ae72944090219719ef25779358b1
WilsoZhang16 <wilso.zhang@mail.utoronto.ca>
2024-03-10 22:32:49 -0400

    fixed method error, cleaned unnecessary comments

--- OneOnOne/calendars/serializers.py
+++ OneOnOne/calendars/serializers.py
@@ -11,30 +11,14 @@ class CalendarSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if self.context['request'].method in ['PUT', 'PATCH']:
+        request = self.context.get('request') if self.context else None
+
+        if request and request.method in ['PUT', 'PATCH']:
             # Restrict fields to 'name' only for update operations
             allowed = {'name'}
             existing = set(self.fields)
             for field_name in existing - allowed:
                 self.fields.pop(field_name)
-
-    # def validate_name(self, value):
-    #     owner = self.context['request'].user
-    #     if Calendar.objects.filter(owner=owner, name=value).exists():
-    #         raise serializers.ValidationError('Calendar with this name already exists')
-    #     return value
-    #
-    # def validate_start_date(self, value):
-    #     try:
-    #         datetime.strptime(str(value), '%Y-%m-%d')
-    #     except ValueError:
-    #         raise serializers.ValidationError('This field expects a date in the format YYYY-MM-DD')
-    #
-    # def validate_end_date(self, value):
-    #     try:
-    #         datetime.strptime(str(value), '%Y-%m-%d')
-    #     except ValueError:
-    #         raise serializers.ValidationError('This field expects a date in the format YYYY-MM-DD')
 
 
 class AvailabilitySerializer(serializers.ModelSerializer):
