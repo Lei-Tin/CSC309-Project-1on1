53a547888c21c4c5caed98cd6f0d6bb692fb8051
WilsoZhang16 <wilso.zhang@mail.utoronto.ca>
2024-03-12 19:40:32 -0400

    documentation update

--- OneOnOne/calendars/serializers.py
+++ OneOnOne/calendars/serializers.py
@@ -4,6 +4,11 @@ from datetime import datetime
 
 
 class CalendarSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(help_text="A name for the calendar")
+    start_date = serializers.CharField(help_text="The starting date for the calendar")
+    end_date = serializers.CharField(help_text="The ending date for the calendar")
+    finalized = serializers.CharField(help_text="Whether a schedule has been finalized for this calendar")
+
     class Meta:
         model = Calendar
         fields = '__all__'
@@ -22,6 +27,11 @@ class CalendarSerializer(serializers.ModelSerializer):
 
 
 class AvailabilitySerializer(serializers.ModelSerializer):
+    user = serializers.CharField(help_text="The user id of the user that created this availability")
+    start_period = serializers.CharField(help_text="The start time for the availability")
+    end_period = serializers.CharField(help_text="The ending time for the availability")
+    preference = serializers.CharField(help_text="The preference level for this availability")
+
     class Meta:
         model = Availability
         fields = '__all__'
@@ -29,6 +39,9 @@ class AvailabilitySerializer(serializers.ModelSerializer):
 
 
 class InviteeSerializer(serializers.ModelSerializer):
+    invitee = serializers.CharField(help_text="The user id of the invited user to the calendar")
+    deadline = serializers.CharField(help_text="The last time that the invitee can add an availability")
+
     class Meta:
         model = Invitee
         fields = '__all__'
@@ -47,6 +60,11 @@ class InviteeSerializer(serializers.ModelSerializer):
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    calendar = serializers.CharField(help_text="The calendar id that the meeting is scheduled under")
+    start_period = serializers.CharField(help_text="The start time for the availability")
+    end_period = serializers.CharField(help_text="The ending time for the availability")
+    meeter = serializers.CharField(help_text="The user id of the user that the owner is meeting with")
+
     class Meta:
         model = Meets
         fields = '__all__'
