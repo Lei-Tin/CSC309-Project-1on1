430de52ad2f692ea40e2c57ee347b3f3b6bb9fc3
HLeiTR <sy.hung@mail.utoronto.ca>
2024-04-04 22:37:13 -0400

    [+] Fixed notification dropdown css and removed reminder

--- OneOnOne/one_on_one/one_on_one_rc/src/components/Layout/Navbar/CustomNavbarLogged/Notification/NotificationItem.js
+++ OneOnOne/one_on_one/one_on_one_rc/src/components/Layout/Navbar/CustomNavbarLogged/Notification/NotificationItem.js
@@ -5,16 +5,18 @@ import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 export const FriendNotificationItem = ({ username, onAccept, onDecline }) => {
   return (
     <div className="dropdown-item">
-      <div className="dropdown-item-content">
-        <p>{username} has added you to their contacts</p>
-      </div>
-      <div className="dropdown-item-actions">
-        <button className="accept" onClick={() => onAccept(username, true)}>
-          <FontAwesomeIcon icon={faCheck} />
-        </button>
-        <button className="decline" onClick={() => onDecline(username, false)}>
-          <FontAwesomeIcon icon={faTimes} />
-        </button>
+      <div className="dropdown-container">
+        <div className="dropdown-item-content">
+          <p>{username} has added you to their contacts</p>
+        </div>
+        <div className="dropdown-item-actions">
+          <button className="accept" onClick={() => onAccept(username, true)}>
+            <FontAwesomeIcon icon={faCheck} />
+          </button>
+          <button className="decline" onClick={() => onDecline(username, false)}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -23,16 +25,18 @@ export const FriendNotificationItem = ({ username, onAccept, onDecline }) => {
 export const InviteNotificationItem = ({ inviter, calendar, onAccept, onDecline }) => {
   return (
     <div className="dropdown-item">
-      <div className="dropdown-item-content">
-        <p>{inviter} has invited you to {calendar}</p>
-      </div>
-      <div className="dropdown-item-actions">
-        <button className="accept" onClick={() => onAccept(inviter, calendar)}>
-          <FontAwesomeIcon icon={faCheck} />
-        </button>
-        <button className="decline" onClick={() => onDecline(inviter, calendar)}>
-          <FontAwesomeIcon icon={faTimes} />
-        </button>
+      <div className="dropdown-container">
+        <div className="dropdown-item-content">
+          <p>{inviter} has invited you to {calendar}</p>
+        </div>
+        <div className="dropdown-item-actions">
+          <button className="accept" onClick={() => onAccept(inviter, calendar)}>
+            <FontAwesomeIcon icon={faCheck} />
+          </button>
+          <button className="decline" onClick={() => onDecline(inviter, calendar)}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
       </div>
     </div>
   );
